4f3658fe8cd880930791a4f15b5e522bb79d9245
taoghetgithub71420 <cotsdow0111@gmail.com>
2021-07-02 11:58:01 +0700

    Add project files.

---
+++ BigSchool/Controllers/CoursesController.cs
@@ -0,0 +1,52 @@
+﻿using BigSchool.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        public ActionResult Create()
+        {
+            //get list category
+            BigSchoolContext context = new BigSchoolContext();
+            Course objCourse = new Course();
+            objCourse.ListCategory = context.Categories.ToList();
+
+            return View(objCourse);
+           
+        }
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Course objCourse)
+        {
+            BigSchoolContext context = new BigSchoolContext();
+
+            //(Kiểm tra dữ liệu nhập phía Server, trước khi lưu vào co sở dữ liệu, nếu nhập sai yêu cầu lỗi sẽ được gửi lại trang đang thao tác)
+            // Không xét valid LectureId vì bằng user đăng nhập
+            // Không xét valid LectureId vì bằng user đăng nhập
+            ModelState.Remove("LecturerId");
+            if (!ModelState.IsValid)
+            {
+                objCourse.ListCategory = context.Categories.ToList();
+                return View("Create", objCourse);
+            }
+            // lay login user ID
+            ApplicationUser user = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
+            objCourse.LecturerId = user.Id;
+
+            // add vao csdl
+            context.Courses.Add(objCourse);
+            context.SaveChanges();
+            // trở về home
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ BigSchool/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using BigSchool.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.Mvc;
+
+namespace BigSchool.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            BigSchoolContext context = new BigSchoolContext();
+            var upcommingCourse = context.Courses.Where(p => p.DataTime > DateTime.Now).OrderBy(p => p.DataTime).ToList();
+            foreach (Course i in upcommingCourse)
+            {
+                ApplicationUser user = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(i.LecturerId);
+                // quản lý user, khi đăng nhập, lấy ID của đứa nào đăng nhập, system.web là những hàm trong thư viện để gọi id tương ứng, findbyID lấy id dựa vào id của user, dựa vào get identity
+                i.Name = user.Name;
+              
+            }
+            return View(upcommingCourse);
+        }
+    }
+
+      
+    
+}
